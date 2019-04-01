32037c353ef411a0210d782c30c8784d42126d49
Rasigavijaya <46052495+Rasigavijaya@users.noreply.github.com>
2019-04-01 16:15:04 +0530

    Update 1.c

--- 1.c
+++ 1.c
@@ -13,6 +13,20 @@ int main()
     {
         for(j=i+1;j<n;j++)
         {
+            if(a[i]!=a[j])
+            {
+                c++;
+            }
+        }
+    }
+    if(c==n)
+    {
+        printf("unique");
+    }
+    for(i=0;i<n;i++)
+    {
+        for(j=i+1;j<n;j++)
+        {
             if(a[i]==a[j] && a[i]!='*')
             {
                 a[j]='*';
