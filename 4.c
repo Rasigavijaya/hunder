be5f8ee6e68b3665fda6d916e91e6946cadb9758
Rasigavijaya <46052495+Rasigavijaya@users.noreply.github.com>
2019-04-01 16:31:51 +0530

    Create 4.c

---
+++ 4.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+void main()
+{
+    int n;
+    scanf("%d",&n);
+    int a[n],i,j,k,c=0;
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        for(j=i+1;j<n;j++)
+        {
+            if(a[i]==a[j])
+            {
+                a[i]='*';
+                a[j]='*';
+            }
+        }
+    }
+    for(i=0;i<n;i++)
+    {
+        if(a[i]!='*')
+        {
+            printf("%d",a[i]);
+        }
+    }
+}
